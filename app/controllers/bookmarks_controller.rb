cd81c658cf744360c2d4fad8c189ce9f61030ae2
Jean-Francois Numainville <jfnumainville@gmail.com>
2022-04-21 22:01:57 -0400

    Finished the routes and front end design of index page

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -6,4 +6,28 @@ class BookmarksController < ApplicationController
     redirect_to list_path(@bookmark.list_id)
 
   end
+
+  def new
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.list_id = @list.id
+
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render 'lists/show'
+    end
+  end
+
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id)
+  end
+
 end
--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -2,25 +2,45 @@ class ListsController < ApplicationController
 
   def index
     @lists = List.all
+    end
+
+  def new
+    @list = List.new
+
+  end
+
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      render "lists/new"
+    end
   end
 
   def show
     @list = List.find(params[:id])
-    @movies = Movie.all
-    @bookmarks = Bookmark.where(list_id: "#{@list.id}")
+    @bookmark = Bookmark.new
   end
 
   private
 
-  def eligible_movies
-    @list = List.find(params[:id])
-    movies_in_list = @list.movies
-    all_movies = Movie.all
-
-
-
-
-
+  def list_params
+    params.require(:list).permit(:name)
 
   end
+
+  # def available_movies
+  #   @list = List.find(params[:id])
+  #   movies_in_list = @list.movies
+  #   available_movies = Movie.all
+
+  #   #Deleting all the movies that are in the list from the list of movies
+  #   movies_in_list.each do |movie|
+  #     available_movies.delete(movie)
+  #   end
+
+  #   return available_movies
+
+  # end
 end
