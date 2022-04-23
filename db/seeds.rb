91f4d29e9a01ccfd3448b1e61582ac123edacab1
Jean-Francois Numainville <jfnumainville@gmail.com>
2022-04-23 15:20:21 -0400

    Fixed cloud in configs

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require "open-uri"
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -16,10 +17,9 @@ p Movie.create(title: "Ocean's Eight", overview: "Debbie Ocean, a criminal maste
 List.destroy_all
 puts "Creating Lists"
 5.times do
-
   p new_list = List.create(name: Faker::Book.genre)
-
-
+  file = URI.open('https://picsum.photos/200/300?random=2')
+  new_list.picture.attach(io: file, filename: 'nes.png', content_type: 'image/png')
  end
 
  movie_ids = []
